0ed1cfa74ba8ea8bb7e18e40b6a28c0af212da97
Trong Hai <truongtronghai@users.noreply.github.com>
2024-02-21 15:05:25 +0700

    Create base64ToBlob.js

---
+++ base64ToBlob.js
@@ -0,0 +1,14 @@
+convertBase64ToBlob: function(strBase64,contentType){
+  const base64Data = strBase64.split(',')[1];
+
+  const byteCharacters = atob(base64Data);
+  const byteNumbers = new Array(byteCharacters.length);
+
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+
+  const byteArray = new Uint8Array(byteNumbers);
+  const blob = new Blob([byteArray], { type: contentType });
+  return blob
+}
