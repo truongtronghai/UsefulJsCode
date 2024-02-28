279ab171218329ff1eff17461ac17491eef50c04
Trong Hai <truongtronghai@users.noreply.github.com>
2024-02-28 11:53:31 +0700

    Update check-ipad.js

--- check-ipad.js
+++ check-ipad.js
@@ -1,1 +1,3 @@
-const isIpad = /iPad/i.test(navigator.userAgent) && navigator.maxTouchPoints > 1
+// iPad will have multitouch and 'iPad' word OR 'Macintosh'
+// if containing 'Macintosh' but not multitouch => not Ipad
+const isIpad = /iPad|Macintosh/i.test(navigator.userAgent) && navigator.maxTouchPoints && navigator.maxTouchPoints > 1
